cd2a6723e7558cfa5395223d071cac481b4666d1
MarisGomez <gomezmarilin614@gmail.com>
2024-11-21 20:25:22 -0300

    resolvi un parcial 2023

--- Parciales y Tp grupal/Parciales viejos Python.py
+++ Parciales y Tp grupal/Parciales viejos Python.py
@@ -151,18 +151,6 @@ print(conventir_a_diccionario([-1,0,4,100,100,-1,-1]))
 ######################################################################################################
 
 #1) Índice de la n-ésima aparición [2 puntos]
-#Guido y Marcela son dos estudiantes de IP, nervioses con el parcial de Python. 
-#Con el objetivo de tener un rato antes del parcial para preguntarse algunas dudas 
-#deciden encontrarse en el colectivo y viajar juntes. 
-#Para poder coordinar de forma exacta en qué colectivo se tienen que subir, 
-#Marcela usa sus habilidades de programación aprendidas en IP para acceder de forma 
-#poco legítima a la base de datos de colectivos de todas las empresas. 
-#Con esto, arma una lista de todos los colectivos que van a pasar por la parada de
-#Guido alrededor del horario acordado y le indica a Guido que se tiene que subir en 
-#el 3er colectivo de la línea 34. Por desgracia, Guido se olvida sus lentes antes 
-#de salir y no es capaz de distinguir a qué línea pertenece cada colectivo que 
-#llega a la parada. Por lo que solo puede contar cantidad total de colectivos que 
-#pasan.
 #Implementar la función ind_nesima_aparicion que dada una secuencia de enteros s, 
 #y dos enteros n y elem devuelve la posición en la cual elem aparece por n-ésima vez 
 #en s. En caso de que elem aparezca menos de n veces en s, devolver -1.
@@ -180,7 +168,161 @@ print(conventir_a_diccionario([-1,0,4,100,100,-1,-1]))
 #elem = 1
 #se debería devolver res = 2
 
-def ind_nesima_aparicion(s: list, n: int, elem: int) -> int:
+def cantidad_apariciones(s: list, n: int) -> int:
     res: int = 0
-    for i in range (0, len(s)):
-        
+    for i in s:
+        if i == n:
+            res += 1
+    return res
+
+def ind_nesima_aparicion(s: list[int], n: int, elem: int) -> int:
+    indice: int = 0
+    contador: int = 0
+    res: int = -1
+
+    if cantidad_apariciones(s,elem) >= n:
+        while contador < n:
+                if s[indice] == elem:
+                     contador = contador + 1
+                indice = indice + 1
+                res = res + 1
+    return res
+
+print(ind_nesima_aparicion([-1, 1, 1, 5, -7, 1, 3],2,1))
+print(ind_nesima_aparicion([-1,0,4,100,100,-1,-1],2,100))
+print(ind_nesima_aparicion([-1,4,0,4,3,0,100,0,-1,-1],3,4))
+
+#2) Mezclar [2 puntos]
+#Implementar la función mezclar que dadas dos listas 
+#s1 y s2 con igual cantidad de elementos devuelva una lista con los elementos 
+#intercalados. Esto es, las posiciones pares de res tendrán los elementos de s1 
+#y las posiciones impares los elementos de s2, respetando el orden original.
+
+#problema mezclar (in s1: seq⟨Z⟩, in s2: seq⟨Z⟩) : seq⟨Z⟩ {
+#  requiere: {|s1| = |s2| }
+#  asegura: {|res| = 2 * |s1|}
+#  asegura: {res[2*i] = s1[i] y res[2*i+1] = s2[i], para i entre 0 y |s1|-1}
+#}
+#TIP: realizar la iteración mediante índices y no mediante elementos
+
+#Por ejemplo, dadas
+#s1 = [1, 3, 0, 1]
+#s2 = [4, 0, 2, 3]
+#se debería devolver res = [1, 4, 3, 0, 0, 2, 1, 3]
+
+def mezclar(s1: list[int], s2: list[int]) -> list[int]:
+    lista_nueva: list[int] = []
+    indice:int = 0
+    indice1:int = 0
+    indice2:int = 0
+    while indice < len(s1 + s2):
+        if indice % 2 == 0:
+            lista_nueva.append(s1[indice1])
+            indice1 += 1
+            indice += 1
+        else:
+            lista_nueva.append(s2[indice2])
+            indice2 += 1
+            indice += 1
+    return lista_nueva
+
+print(mezclar([1, 3, 0, 1],[4, 0, 2, 3]))
+
+#3) A los pingos: resultado carreras [3 puntos]
+#Implementar la función frecuencia_posiciones_por_caballo que dada la lista de 
+#caballos que corrieron las carreras, y el diccionario que tiene los resultados del 
+#hipódromo en el formato carreras:posiciones_caballos, donde carreras es un String 
+#y posiciones_caballos es una lista de strings con los nombres de los caballos, 
+#genere un diccionario de caballos:#posiciones, que para cada caballo devuelva la 
+#lista de cuántas veces salió en esa posición.
+
+#Tip: para crear una lista con tantos ceros como caballos se puede utilizar la siguiente 
+#sintaxis lista_ceros = [0]*len(caballos)
+
+#problema frecuencia_posiciones_por_caballo(in caballos: seq⟨String⟩, in carreras: 
+#dict⟨String,seq⟨String⟩⟩: dict⟨String,seq⟨Z⟩⟩ {
+#  requiere: {caballos no tiene repetidos}
+#  requiere: {Los valores del diccionario carreras son permutaciones de la lista 
+#  caballos (es decir, tienen exactamente los mismos elementos que caballos, en 
+#  cualquier orden posible)}
+#  asegura: {res tiene como claves los elementos de caballos}
+#  asegura: {El valor en res de un caballo es una lista que indica en la posición i 
+#  cuántas veces salió ese caballo en la i-ésima posición.}
+#}
+#Por ejemplo, dados
+#caballos= ["linda", "petisa", "mister", "luck" ]
+#carreras= {"carrera1":["linda", "petisa", "mister", "luck"],
+#           "carrera2":["petisa", "mister", "linda", "luck"]}
+#se debería devolver res = {"petisa": [1,1,0,0],
+#                           "mister": [0,1,1,0],
+#                            "linda": [1,0,1,0],
+#                             "luck": [0,0,0,2]}
+
+def posicion(caballo: str, carrera:list) -> int:
+    indice: int = 0
+    for c in carrera:
+        if c == caballo:
+            return indice
+        indice += 1
+
+def posiciones(caballo: str, carreras: dict) -> [int]:
+    lista_carreras: list = []
+
+    for carrera in carreras.keys():
+        lista_carreras.append(carreras[carrera])
+
+    primera_carrera: list = lista_carreras[0]
+    caballos: int = len(primera_carrera)
+    res: list = [0] * caballos
+
+    for lista in lista_carreras:
+        res[posicion(caballo,lista)] += 1
+    return res
+
+def frecuencia_posiciones_por_caballo(caballos: list, carreras: dict) -> dict:
+    res: dict = {}
+    for caballo in caballos:
+        res[caballo] = posiciones(caballo, carreras)
+    return res
+
+caballos= ["linda", "petisa", "mister", "luck" ]
+carreras= {"carrera1":["linda", "petisa", "mister", "luck"], "carrera2":["petisa", "mister", "linda", "luck"]}
+print(frecuencia_posiciones_por_caballo(caballos, carreras))
+
+#4) Matriz capicúa [3 puntos]
+#Implementar la función matriz_capicua que dada una matriz devuelve True si 
+#cada una de sus filas es capicúa. Es decir, si cada fila es igual leída de 
+#izquierda a derecha o de derecha a izquierda. Definimos a una secuencia de
+#secuencias como matriz si todos los elemento de la primera secuencia tienen 
+#la misma longitud.
+
+#problema matriz_capicua(in m:seq⟨seq⟨Z⟩⟩ ) : Bool {
+#  requiere: {todos los elementos de m tienen igual longitud (los elementos de m son secuencias)}
+#  asegura: {(res = true) <=> todos los elementos de m son capicúa}
+#}
+
+#Por ejemplo, dada la matriz
+#m = [[1,2,2,1],[-5,6,6,-5],[0,1,1,0]]
+#se debería devolver res = true
+
+def capicua(s: list[int]) -> bool:
+    if len(s) == 0 or len(s) == 1:
+        return True
+    else:
+        indice:int = 0
+        while indice < len(s):
+            if s[indice] != s[len(s) - 1 - indice]:
+                return False
+            indice += 1
+        return True
+    
+def matriz_capicua(m: list[list[int]]) -> bool:
+    res: bool = True
+    for s in m:
+        if not capicua(s):
+            res = False
+    return res
+
+print(matriz_capicua([[1,2,2,1],[-5,6,6,-5],[0,1,1,0]]))
+
+######################################################################################################
