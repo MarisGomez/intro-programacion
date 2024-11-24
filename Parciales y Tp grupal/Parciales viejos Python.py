a28b5c4b94d5587f024fc219665097ae79864196
MarisGomez <gomezmarilin614@gmail.com>
2024-11-24 12:26:30 -0300

    resolvi un parcial 2024

--- Parciales y Tp grupal/Parciales viejos Python.py
+++ Parciales y Tp grupal/Parciales viejos Python.py
@@ -1,3 +1,7 @@
+from queue import LifoQueue as Pila
+from queue import Queue as Cola
+c = Cola
+p = Pila
 # Ejercicio 1
 #
 #  problema ultima_aparicion (s: seq⟨Z⟩, e: Z) : Z {
@@ -265,7 +269,7 @@ def posicion(caballo: str, carrera:list) -> int:
             return indice
         indice += 1
 
-def posiciones(caballo: str, carreras: dict) -> [int]:
+def posiciones(caballo: str, carreras: dict) -> list[int]:
     lista_carreras: list = []
 
     for carrera in carreras.keys():
@@ -326,3 +330,159 @@ def matriz_capicua(m: list[list[int]]) -> bool:
 print(matriz_capicua([[1,2,2,1],[-5,6,6,-5],[0,1,1,0]]))
 
 ######################################################################################################
+#1) Alerta Enfermedades Infecciosas (3 puntos)
+#Necesitamos detectar la aparición de posibles epidemias. Para esto contamos con un lista de enfermedades infecciosas y los registros de atención por guardia dados por una lista expedientes. 
+#Cada expediente es una tupla con ID paciente y enfermedad que motivó la atención. Debemos devolver un diccionario cuya clave son las enfermedades infecciosas y su valor es la proporción de pacientes que se atendieron por esa enfermedad. 
+#En este diccionario deben aparecer solo aquellas enfermedades infecciosas cuya proporción supere cierto umbral.
+
+#problema alarma_epidemiologica (registros: seq⟨ZxString⟩, infecciosas: seq⟨String⟩, umbral: R) : dict⟨String,R⟩ {
+#  requiere: {0 < umbral < 1}
+#  asegura: {claves de res pertenecen a infecciosas}
+#  asegura: {Para cada enfermedad perteneciente a infecciosas, si el porcentaje de pacientes que se atendieron por esa enfermedad sobre el total de registros es mayor o igual al umbral, entonces res[enfermedad] = porcentaje}
+#  asegura: {Para cada enfermedad perteneciente a infecciosas, si el porcentaje de e pacientes que se atendieron por esa enfermedad sobre el total de registros es menor que el umbral, entonces enfermedad no aparece en res}
+#}
+
+def proporcion_infeccion(enfermedad: str, registros: list[tuple[int, str]]) -> float:
+    pacientes_totales: int = len(registros)
+    infectados_por_enfermedad: int = 0
+    for expediente in registros:
+        if expediente[1] == enfermedad:
+            infectados_por_enfermedad += 1
+    porcentaje: float = (infectados_por_enfermedad/pacientes_totales)
+    return porcentaje
+
+def alarma_epidemiologica(registros: list[tuple[int, str]], infecciosas: list[str], umbral: float) -> dict[str, float]:
+    res: dict[str, float] = {}
+    for enfermedad in infecciosas:
+        if not pertenece(enfermedad, res):
+            if proporcion_infeccion(enfermedad, registros) >= umbral:
+                res[enfermedad] = proporcion_infeccion(enfermedad, registros)
+    return res
+
+registros = [["juan","viruela"],["lucas","covid"],["alex","gripe"],["laura","covid"],["pablo","covid"]]
+infecciosas = ["viruela","covid","gripe"]
+print(proporcion_infeccion("covid", registros))
+print(alarma_epidemiologica(registros,infecciosas,0.5))
+
+#2) Orden de atención (1 punto)
+#Desde el Hospital Fernandez nos pidieron solucionar una serie de problemas relacionados con la información que maneja sobre los pacientes y el personal de salud. 
+#En primer lugar debemos resolver en qué orden se deben atender los pacientes que llegan a la guardia. 
+#En enfermería, hay una primera instancia que clasifica en dos colas a los pacientes: una urgente y otra postergable (esto se llama hacer triage). 
+#A partir de dichas colas que contienen la identificación del paciente, se pide devolver una nueva cola según la siguiente especificación.
+
+#problema orden_de_atencion ( in urgentes: cola⟨Z⟩, in postergables: cola⟨Z⟩) : cola⟨Z⟩ {
+#  requiere: {no hay elementos repetidos en urgentes}
+#  requiere: {no hay elementos repetidos en postergables}
+#  requiere: {la intersección entre postergables y urgentes es vacía}
+#  requiere: {|postergables| = |urgentes|}
+#  asegura: {no hay repetidos en res }
+#  asegura: {res es permutación de la concatenación de urgentes y postergables}
+#  asegura: {Si urgentes no es vacía, en tope de res hay un elemento de urgentes}
+#  asegura: {En res no hay dos seguidos de urgentes}
+#  asegura: {En res no hay dos seguidos de postergables}
+#  asegura: {Para todo c1 y c2 de tipo "urgente" pertenecientes a urgentes si c1 aparece antes que c2 en urgentes entonces c1 aparece antes que c2 en res}
+#  asegura: {Para todo c1 y c2 de tipo "postergable" pertenecientes a postergables si c1 aparece antes que c2 en postergables entonces c1 aparece antes que c2 en res}
+
+def orden_de_atencion(urgentes:Cola[int], postergables:Cola[int]) -> Cola[int]:
+    orden_final: Cola = Cola()
+    contenedor_urg: Cola = Cola()
+    contenedor_pos: Cola = Cola()
+    while not urgentes.empty() and not postergables.empty():
+        orden_final.put(urgentes.get())
+        orden_final.put(postergables.get())
+        contenedor_urg.put(urgentes.get())
+        contenedor_pos.put(postergables.get())
+    while not contenedor_urg.empty() and not contenedor_pos.empty():
+        urgentes.put(contenedor_urg.get())
+        postergables.put(contenedor_pos.get())
+    return orden_final
+
+#3) Camas ocupadas en el hospital (2 puntos)
+#Queremos saber qué porcentaje de ocupación de camas hay en el hospital. El hospital se representa por una matriz en donde las filas son los pisos, y las columnas son las camas. 
+#Los valores de la matriz son booleanos que indican si la cama está ocupada o no. Si el valor es verdadero (True) indica que la cama está ocupada. 
+#Se nos pide programar en Python una función que devuelve una secuencia de enteros, indicando la proporción de camas ocupadas en cada piso.
+
+#problema nivel_de_ocupacion(camas_por_piso:seq⟨seq⟨Bool⟩⟩) : seq⟨R⟩ {
+#  requiere: {Todos los pisos tienen la misma cantidad de camas.}
+#  requiere: {Hay por lo menos 1 piso en el hospital.}
+#  requiere: {Hay por lo menos una cama por piso.}
+#  asegura: {|res| = |camas|}
+#  asegura: {Para todo 0<= i < |res| se cumple que res[i] es igual a la cantidad de camas ocupadas del piso i dividido el total de camas del piso i)}
+#}
+
+def proporcion_ocupacion_fila(camas_por_piso: list[bool]) -> float:
+    camas_ocupadas: int = 0
+    camas_totales: int = len(camas_por_piso)
+    for cama in range(0,len(camas_por_piso)):
+        if camas_por_piso[cama] == True:
+            camas_ocupadas += 1
+    res = (camas_ocupadas/camas_totales) 
+    return res
+
+def nivel_de_ocupacion(camas_por_piso: list[list[bool]]) -> list[float]:
+    res: list[float] = []
+    for piso in camas_por_piso:
+        res.append(proporcion_ocupacion_fila(piso))
+    return res
+
+print(nivel_de_ocupacion([[True,False,False,False],[True,False,False,True],[False,False,False,False]]))
+
+# OTRA FORMA
+def nivel_de_ocupacion2(camas_por_piso:list[list[bool]]) -> list[float]:
+    res : list[float] = []
+    contador : int = 0
+    cantidad_camas = len(camas_por_piso)
+    for fila in range(0,len(camas_por_piso)) : 
+        for elem in range(0,len(camas_por_piso)):
+            if camas_por_piso[fila][elem] == True:
+                contador += 1
+        res.append(contador/cantidad_camas)
+        contador = 0
+    return res
+
+#4) Quiénes trabajaron más? (2 puntos)
+#Dado un diccionario con la cantidad de horas trabajadas por empleado, en donde la clave es el ID del empleado y el valor es una lista de las horas trabajadas por día, 
+#queremos saber quienes trabajaron más para darles un premio. Se deberá buscar la o las claves para la cual se tiene el máximo valor de cantidad total de horas, y devolverlas en una lista.
+
+#problema empleados_del_mes(horas:dicc⟨Z,seq⟨Z⟩⟩) : seq⟨Z⟩ {
+#  requiere: {No hay valores en horas que sean listas vacías}
+#  asegura: {Si ID pertence a res entonces ID pertence a las claves de horas}
+#  asegura: {Si ID pertenece a res, la suma de sus valores de horas es el máximo de la suma de elementos de horas de todos los otros IDs}
+#  asegura: {Para todo ID de claves de horas, si la suma de sus valores es el máximo de la suma de elementos de horas de los otros IDs, entonces ID pertences a res}
+#}
+
+def sumatoria(s:list[int]) -> int:
+    res: int = 0
+    for n in s:
+        res += n
+    return res
+
+def maximo (s:list[int])-> int : 
+    mayor : int = 0
+    for num in s : 
+        if num > mayor:
+            mayor = num
+    return mayor
+
+def pos_mayor(s:list[int]) -> list[int] : 
+    mayor : int = maximo(s)
+    res : list[int] = []
+    for i in range(0,len(s)): 
+        if s[i] == mayor: 
+            res.append(i)
+    return res
+
+def empleados_del_mes(horas:dict[int, list[int]]) -> list[int]:
+    lista_trabajadores: list[int] = []
+    horas_por_trabajador: list[int] = []
+    res: list[int] = []
+    for ids in horas.keys():
+        lista_trabajadores.append(ids)
+        horas_por_trabajador.append(sumatoria(horas[ids]))
+    mas_horas = pos_mayor(horas_por_trabajador)
+    for i in range(0,len(mas_horas)):
+        res.append(lista_trabajadores[mas_horas[i]])
+    return res
+
+horas = {111:[5,3,7],112:[9,8,8],113:[3,5,4],114:[2,5,1]}
+print(empleados_del_mes(horas))
