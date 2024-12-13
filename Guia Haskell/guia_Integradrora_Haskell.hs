ea370331728abfbf69cffee539ccf7bd127ea7c9
MarisGomez <gomezmarilin614@gmail.com>
2024-12-13 16:50:15 -0300

    rehago la guia integradora haskell

--- Guia Haskell/guia_Integradrora_Haskell.hs
+++ Guia Haskell/guia_Integradrora_Haskell.hs
@@ -1,4 +1,4 @@
--- EJ 1
+-- Ejercicio 1
 {-
 problema generarStock (productos: seq⟨String⟩) : seq⟨String × Z⟩ {
 requiere: {True}
@@ -8,34 +8,36 @@ res[i]1 es igual a la cantidad de veces que aparece producto en productos}
 }
 -}
 
-productos :: [String] ->[(String, Int)]
-productos [] = []
-productos [x] = [(x, 1)]
-productos (x:xs) = (x, cantApariciones x (x:xs)) : productos (quitarTodos x xs)
+stockPorProducto :: [String] -> String -> Int
+stockPorProducto [] _ = 0
+stockPorProducto (x:xs) p | x == p = 1 + stockPorProducto xs p
+                          | otherwise = stockPorProducto xs p
 
-pertenece :: String -> [String] -> Bool
-pertenece _ [] = False
-pertenece p (x:xs) | p == x = True
-                   | otherwise = pertenece p xs
+eliminarTodos :: (Eq t) => [t] -> t -> [t]
+eliminarTodos [] _ = []
+eliminarTodos (x:xs) i | x == i = eliminarTodos xs i
+                       | otherwise = x : eliminarTodos xs i
 
-quitarTodos :: String -> [String] -> [String]
-quitarTodos _ [] = []
-quitarTodos p (x:xs) | p == x && not (pertenece p xs) = xs
-                     | p == x && pertenece p xs = quitarTodos p xs
-                     | otherwise = x : quitarTodos p xs
+generarStock :: [String] ->[(String, Int)]
+generarStock [] = []
+generarStock (x:xs) = (x, stockPorProducto (x:xs) x) : generarStock (eliminarTodos xs x)
 
-cantApariciones :: String -> [String] -> Int
-cantApariciones _ [] = 0
-cantApariciones p (x:xs) | p == x = 1 + cantApariciones p xs
-                         | otherwise = 0 + cantApariciones p xs
+-- Ejercicio 2
+{-
+problema stockDeProducto (stock: seq⟨String × Z⟩, producto: String ) : Z {
+requiere: {No hay productos repetidos en stock}
+requiere: {Todas las cantidades (segundas componentes) de stock son mayores a cero}
+asegura: {(res = 0 y producto no se encuentra en el stock) o (existe un i tal que 0 ≤ i < |stock| y producto = stock[i]0
+y res = stock[i]1}
+}
+-}
 
--- EJ 2
 stockDeProducto :: [(String, Int)] -> String -> Int
 stockDeProducto [] _ = 0
-stockDeProducto ((producto, stock):xs) p | p == producto = stock
+stockDeProducto ((producto, stock):xs) p | producto == p = stock
                                          | otherwise = stockDeProducto xs p
 
--- EJ 3
+-- Ejercicio 3
 {-
 problema dineroEnStock (stock: seq⟨String × Z⟩, precios: seq⟨String × R⟩ ) : R {
 requiere: {No hay productos repetidos en stock}
@@ -47,16 +49,87 @@ asegura: {res es igual a la suma de los precios de todos los productos que est´
 de cada producto que hay en stock}
 }
 -}
+
+precioPorProducto :: [(String, Float)] -> String -> Float
+precioPorProducto [] _ = 0
+precioPorProducto ((producto, precio):xs) p | producto == p = precio
+                                            | otherwise = precioPorProducto xs p
+
 dineroEnStock :: [(String, Int)] -> [(String, Float)] -> Float
 dineroEnStock [] _ = 0
-dineroEnStock ((producto, stock):xs) precios = precioProducto producto precios * fromIntegral (stockDeProducto ((producto, stock):xs) producto ) + dineroEnStock xs precios
+dineroEnStock ((producto,stock):xs) precios = precioPorProducto precios producto * fromIntegral stock + dineroEnStock xs precios
 
-precioProducto :: String -> [(String,Float)] -> Float
-precioProducto producto ((p, precio):xs) |producto == p = precio
-                                         |otherwise = precioProducto producto xs
 
--- EJ 4
-aplicarOferta :: [(String, Int)] ->[(String, Float)] ->[(String,Float)]
+-- Ejercicio 4
+{-
+problema aplicarOferta (stock: seq⟨String × Z⟩, precios: seq⟨String × R⟩ ) : seq⟨String × R⟩ {
+requiere: {No hay productos repetidos en stock}
+requiere: {No hay productos repetidos en precios}
+requiere: {Todas las cantidades (segundas componentes) de stock son mayores a cero}
+requiere: {Todas las precios (segundas componentes) de precios son mayores a cero}
+requiere: {Todo producto de stock aparece en la lista de precios}
+asegura: {|res| = |precios|}
+asegura: {Para todo 0 ≤ i < |precios|, si stockDeProducto(stock, precios[i]0) > 10, entonces res[i]0 = precios[i]0 y
+res[i]1 = precios[i]1∗ 0,80}
+asegura: {Para todo 0 ≤ i < |precios|, si stockDeProducto(stock, precios[i]0) ≤ 10, entonces res[i]0 = precios[i]0 y
+res[i]1 = precios[i]1 }
+}
+-}
+
+aplicarOferta :: [(String, Int)] -> [(String, Float)] -> [(String,Float)]
 aplicarOferta [] _ = []
-aplicarOferta ((producto, stock):xs) precios | (stockDeProducto ((producto, stock):xs) producto) > 10 = (producto, precioProducto producto precios * 0.8) : aplicarOferta xs precios
-                                             | otherwise = (producto, precioProducto producto precios) : aplicarOferta xs precios
+aplicarOferta ((producto, stock):xs) precios | stock > 10 = (producto, precioPorProducto precios producto*0.80) : aplicarOferta xs precios
+                                             | otherwise = (producto, precioPorProducto precios producto) : aplicarOferta xs precios
+
+
+--------------- SOPA DE LETRAS
+-- Ejercicio 5
+{-
+Fila = seq⟨Z⟩
+Tablero = seq⟨F ila⟩
+Posicion = Z × Z – Observaci´on: las posiciones son: (fila, columna)
+Camino = seq⟨P osicion⟩
+
+problema maximo (t: Tablero) : Z {
+requiere: {El tablero t es un tablero bien formado, es decir, la longitud de todas las filas es la misma, y tienen al
+menos un elemento}
+requiere: {Existe al menos una columna en el tablero t }
+requiere: {El tablero t no es vac´ıo, todos los n´umeros del tablero son positivos, mayor estricto a 0}
+asegura: {res es igual al n´umero m´as grande del tablero t}
+}
+-}
+
+maximoFila :: [Int] -> Int
+maximoFila [x] = x
+maximoFila (x:y:xs) | x > y = maximoFila (x:xs)
+                  | otherwise = maximoFila (y:xs)
+
+maximo :: [[Int]] -> Int
+maximo [x] = maximoFila x
+maximo (x:y:xs) | maximoFila x > maximoFila y = maximo (x:xs)
+                | otherwise = maximo (y:xs)
+
+-- Ejercicio 6
+reunirTodos :: [[Int]] -> [Int]
+reunirTodos [x] = x
+reunirTodos (x:y:xs) = x ++ y ++ reunirTodos xs
+
+cantidadApariciones :: [Int] -> Int -> Int
+cantidadApariciones [] _ = 0
+cantidadApariciones (x:xs) i | x == i = 1 + cantidadApariciones xs i
+                             | otherwise = cantidadApariciones xs i
+
+cantidadAparicionesxElem :: [Int] -> [(Int, Int)]
+cantidadAparicionesxElem [] = [] 
+cantidadAparicionesxElem (x:xs) = (x, cantidadApariciones (x:xs) x) : cantidadAparicionesxElem (eliminarTodos xs x) 
+
+maxCantAparicionesxElem :: [(Int, Int)] -> Int
+maxCantAparicionesxElem [x] = fst x
+maxCantAparicionesxElem (x:y:xs) | snd x > snd y = maxCantAparicionesxElem (x:xs)
+                                 | otherwise = maxCantAparicionesxElem (y:xs)
+
+masRepetido :: [[Int]] -> Int
+masRepetido xs = maxCantAparicionesxElem (cantidadAparicionesxElem (reunirTodos xs))
+
+-- Ejercicio 7
+--valoresDeCamino :: [[Int]] -> [(Int, Int)] -> [Int]
